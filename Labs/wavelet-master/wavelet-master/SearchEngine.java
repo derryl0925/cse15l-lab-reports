e8408dffbc372dbd0231762cae9c72556960e2f8
derryl0925 <116408020+derryl0925@users.noreply.github.com>
2023-06-05 17:21:45 -0700

    Add files via upload

---
+++ Labs/wavelet-master/wavelet-master/SearchEngine.java
@@ -0,0 +1,12 @@
+import java.net.URI;
+class SearchEngine implements URLHandler {
+    public String handleRequest(URI url) {
+        
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
